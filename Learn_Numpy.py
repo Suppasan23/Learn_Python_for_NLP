baf1f9d2728a6d19152a5fc607b8f3a902b6a4cf
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2025-06-30 19:55:44 +0700

    yrrrr

--- Learn_Numpy.py
+++ Learn_Numpy.py
@@ -1,13 +1,6 @@
 import numpy as np
 
-a = np.array([[5,1,7],
-              [9,3,4],
-              [8,2,7]])
+a = np.array([[1,2,3,4],
+              [5,6,7,8]])
 
-b = np.array([[10,20,30],
-              [40,50,90],
-              [80,100,5]])
- 
-t = a.dot(b)
-
-print(t)
+print(a>3)
