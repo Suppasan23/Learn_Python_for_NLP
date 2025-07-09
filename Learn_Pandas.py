928257e5477c49a772d0e3e1d0b391c3045aebe6
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2025-07-09 19:54:51 +0700

    rrrr

--- Learn_Pandas.py
+++ Learn_Pandas.py
@@ -1,17 +1,7 @@
 import pandas as pd
+import numpy as np
 
-name = ["คีย์บอร์ด","เมาส์","ตุ๊กตา"]
-category = ["อุปกรณ์คอม","อุปกรณ์คอม","ของเล่น"]
-price = [500,300,900]
+x = np.arange('2025-02','2025-03', dtype='datetime64[D]')
 
-s_name = pd.Series(name)
-s_category = pd.Series(category)
-s_price = pd.Series(price)
 
-frame = {"ชื่อสินค้า":s_name,"หมวดหมู่":s_category,"ราคา":s_price}
-
-df = pd.DataFrame(frame)
-
-print(df)
-
-df.to_csv("pandas_products.csv")
+print(x)
