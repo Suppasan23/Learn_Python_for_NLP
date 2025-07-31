201b0df2d1fb1f03c8b1dbaaa8352de2929ad217
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2025-07-31 21:54:24 +0700

    sss

--- Learn_Pandas.py
+++ Learn_Pandas.py
@@ -3,18 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # โหลดไฟล์ Excel
-df = pd.read_excel("data/Stock.xlsx", index_col="Name")
+df = pd.read_excel("data/Stock.xlsx",index_col="Name")
 
-# ข้อมูลเพิ่มเติม
-supplement_product_data = [["แพนด้า", "สัตว์", 5000, 8],
-                          ["ฮิปโป", "สัตว์", 8000, 4]]
-supplement_product_column = ["Name", "Category", "Price", "Amount"]
+df["total"] = df["Price"]*df["Amount"]
 
-# สร้าง DataFrame ใหม่
-new_df = pd.DataFrame(data=supplement_product_data, columns=supplement_product_column)
-new_df.set_index("Name", inplace=True)
-
-# รวมข้อมูล
-df = pd.concat([df, new_df])
-
-print(df)
+print(df.sum())
