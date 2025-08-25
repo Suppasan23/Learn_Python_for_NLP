da47190f4becc7ee40968c01cfc4af1d56ae96df
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2025-08-25 16:33:05 +0700

    fefe

--- Learn_SciPy_General.py
+++ Learn_SciPy_General.py
@@ -1,12 +1,32 @@
-import numpy as np
-from scipy.spatial import ConvexHull
-from scipy.integrate import quad, nquad, dblquad, tplquad
-from scipy.optimize import root
-import matplotlib.pyplot as plt
+import sympy as sp
 
-def func(x):
-  return x + np.sin(x)
-input = 0.1
-result = root(func, input)
+# กำหนดตัวแปร
+t, x, h = sp.symbols('t x h')
 
-print(result)
+# นิยามฟังก์ชัน g(t)
+g = t / (2*t + 6)
+
+# (a) g(0)
+a = g.subs(t, 0)
+
+# (b) g(-3)
+b = g.subs(t, -3)
+
+# (c) g(10)
+c = g.subs(t, 10)
+
+# (d) g(x^2)
+d = g.subs(t, x**2)
+
+# (e) g(t + h)
+e = g.subs(t, t + h)
+
+# (f) g(t^2 - 3t + 1)
+f = g.subs(t, t**2 - 3*t + 1)
+
+print("(a) g(0) =", a)
+print("(b) g(-3) =", b)
+print("(c) g(10) =", c)
+print("(d) g(x^2) =", d)
+print("(e) g(t+h) =", e)
+print("(f) g(t^2 - 3t + 1) =", f)
